5df6d81b2b53f673a46c4a98ed393a2f157553e0
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-01-31 16:38:20 +0900

    [refact] SessionUser 에서의 사용자 id 추가

--- src/main/java/Backend/sculptor/User/Entity/SessionUser.java
+++ src/main/java/Backend/sculptor/User/Entity/SessionUser.java
@@ -3,12 +3,16 @@ package Backend.sculptor.User.Entity;
 
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class SessionUser {
     private String name;
     private String profile_image;
+    private UUID id;
 
     public SessionUser(Users user) {
+        this.id = user.getId();
         this.name = user.getName();
         this.profile_image = user.getProfile_image();
     }
--- src/main/java/Backend/sculptor/User/Repository/UserRepository.java
+++ src/main/java/Backend/sculptor/User/Repository/UserRepository.java
@@ -4,7 +4,10 @@ import Backend.sculptor.User.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<Users,Long> {
     public Optional<Users> findByName(String name);
+
+    public Optional<Users> findById(UUID uuid);
 }
