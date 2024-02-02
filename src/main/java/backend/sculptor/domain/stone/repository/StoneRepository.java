539e32f37ca2c2fdfc0ff8c1190e58630e8081a5
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-02-02 21:10:52 +0900

    [feat] .

--- src/main/java/backend/sculptor/domain/stone/repository/StoneRepository.java
+++ src/main/java/backend/sculptor/domain/stone/repository/StoneRepository.java
@@ -2,13 +2,20 @@ package backend.sculptor.domain.stone.repository;
 
 import backend.sculptor.domain.stone.entity.Stone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface StoneRepository extends JpaRepository<Stone, Long> {
-    List<Stone> findByUserId(String userId);
-    List<Stone> findByUserIdAndCategory(String userId, String category);
+public interface StoneRepository extends JpaRepository<Stone, UUID> {
+    @Query("SELECT s FROM Stone s WHERE s.users.id = :userId")
+    List<Stone> findByUserId(@Param("userId") String userId);
+    //List<Stone> findByUserIdAndCategory(String userId, String category);
+
+    @Query("SELECT s FROM Stone s JOIN s.users u WHERE u.id = :userId AND s.category = :category")
+    List<Stone> findByUserIdAndCategory(@Param("userId") String userId, @Param("category") String category);
 
 }
