1f50b653d8c8dcf3c9a1f2796982abf4e103c3f2
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-02-02 19:40:55 +0900

    [refact] Application 파일 위치 리팩토링

--- src/main/java/backend/sculptor/SculptorApplication.java
+++ src/main/java/backend/sculptor/SculptorApplication.java
@@ -1,4 +1,4 @@
-package backend.sculptor.domain;
+package backend.sculptor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
