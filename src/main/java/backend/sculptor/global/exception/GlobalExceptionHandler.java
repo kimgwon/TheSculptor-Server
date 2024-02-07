96b4452a8e2233a5e583dce11015a9cfab071f1a
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-02-07 21:30:36 +0900

    [refactor] 예외처리 리팩토링

--- src/main/java/backend/sculptor/global/exception/GlobalExceptionHandler.java
+++ src/main/java/backend/sculptor/global/exception/GlobalExceptionHandler.java
@@ -2,58 +2,42 @@ package backend.sculptor.global.exception;
 
 import backend.sculptor.global.api.APIBody;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.ResourceAccessException;
 
-import java.net.ConnectException;
 import java.rmi.ServerException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ConnectException.class)
-    public ResponseEntity<APIBody<?>> handleNetworkException(ConnectException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(APIBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "네트워크 연결에 문제가 발생했습니다.", null));
+
+    // 400
+    @ExceptionHandler(BadRequestException.class)
+    public APIBody<?> handleBadRequestException(BadRequestException e) {
+        return e.getErrorResponse();
     }
 
-    @ExceptionHandler(ServerException.class)
-    public ResponseEntity<APIBody<?>> handleServerException(ServerException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(APIBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에서 오류가 발생했습니다.", null));
-    }
-
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<APIBody<Nullable>> handleException(Exception e) {
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                .body(new APIBody<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), null));
-//    }
-
+    // 401
     @ExceptionHandler(SessionExpiredException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ResponseEntity<APIBody<Nullable>> handleSessionExpiredException(SessionExpiredException e) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        // 세션 만료 여부 확인
-        boolean isSessionExpired = authentication == null || !authentication.isAuthenticated();
-
-        if (isSessionExpired) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(APIBody.of(HttpStatus.UNAUTHORIZED.value(), "세션이 만료되었습니다.", null));
-        }
-
-        APIBody<Nullable> errorResponse = APIBody.of(404, e.getMessage(), null);
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    public APIBody<?> handleSessionExpiredException(SessionExpiredException e) {
+        return e.getErrorResponse();
     }
 
+    // 404
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<APIBody<Nullable>> handleNotFoundException(NotFoundException e) {
-        APIBody<Nullable> errorResponse = APIBody.of(404, e.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    public APIBody<?> handleNotFoundException(NotFoundException e) {
+        return e.getErrorResponse();
+    }
+
+    // 500
+    @ExceptionHandler(ResourceAccessException.class)
+    public APIBody<?> handleNetworkException(ResourceAccessException e) {
+        return APIBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "네트워크 연결 실패", null);
+    }
+
+    // 500
+    @ExceptionHandler(ServerException.class)
+    public APIBody<?> handleServerException(ServerException e) {
+        return APIBody.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류 발생", null);
     }
 }
--- src/main/java/backend/sculptor/global/exception/NotFoundException.java
+++ src/main/java/backend/sculptor/global/exception/NotFoundException.java
@@ -1,15 +1,16 @@
 package backend.sculptor.global.exception;
 
 import backend.sculptor.global.api.APIBody;
-import io.micrometer.common.lang.Nullable;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class NotFoundException extends RuntimeException {
-    private final transient APIBody<Nullable> errorResponse;
+    private final transient APIBody<?> errorResponse;
 
     public NotFoundException(String message) {
         super(message);
-        this.errorResponse = APIBody.of(404, message, null);
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        this.errorResponse = APIBody.of(status.value(), message, null);
     }
 }
