1142fce7c0e1d63c3f2f1d7b35119ad91f6a0990
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-02-09 17:27:11 +0900

    [feat] NOT_USER_STONE 추가

--- src/main/java/backend/sculptor/global/exception/ErrorCode.java
+++ src/main/java/backend/sculptor/global/exception/ErrorCode.java
@@ -8,7 +8,8 @@ public enum ErrorCode {
     STONE_NOT_FOUND("해당 돌을 찾을 수 없습니다"),
     COMMENT_NOT_FOUND("해당 방명록을 찾을 수 없습니다"),
 
-    STONE_NOT_COMPLETE("박물관에서 목표일이 지나지 않은 돌은 조회할 수 없습니다");
+    STONE_NOT_COMPLETE("박물관에서 목표일이 지나지 않은 돌은 조회할 수 없습니다"),
+    NOT_USER_STONE("해당 유저의 돌이 아닙니다");
 
     private final String message;
 
